ad884d48555f2a896c393fa9358b40dd05677811
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2026-01-26 14:18:59 -0800

    remove netlify vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import path from "node:path"
-import netlify from "@netlify/vite-plugin"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), netlify()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
