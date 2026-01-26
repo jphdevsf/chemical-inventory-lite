34009a4247d3ce2b75a6a758b2fc7cadd7169ac7
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2026-01-26 13:57:13 -0800

    set up base configs for netlify and neon DB

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "node:path"
+import netlify from "@netlify/vite-plugin"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), netlify()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
