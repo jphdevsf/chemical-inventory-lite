834da3c812c49de252abdacba2be42dfbad9d8ee
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2026-01-26 14:50:49 -0800

    reduce configuration causing conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import path from "node:path"
-import netlify from "@netlify/vite-plugin"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), netlify()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
   },
-  server: { proxy: { "/data": "http://localhost:3001" } },
-  base: "/"
+  server: { proxy: { "/data": "http://localhost:3001" } }
 })
